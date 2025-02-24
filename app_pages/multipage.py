7b92a14e1c4273ea5ce4397ab50f611b08577f92
Pooja  Hirani <pooja.hirani93@gmail.com>
2025-02-24 16:32:32 +0000

    added each pages to the app

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -22,6 +22,14 @@ class MultiPage:
         page = st.sidebar.radio("Navigation", self.pages, format_func=lambda page: page["title"])
         page["function"]()
 
-#multipage app instance
+# multipage app instance
 app = MultiPage("Heritage Housing Analysis")
 
+# Pages to the app
+app.add_page("Project Summary", page_summary_body)
+app.add_page("Data Loading", page_data_loading_body)
+app.add_page("Data Visualization", page_data_visualization_body)
+app.add_page("Hypothesis Validation", page_hypothesis_body)
+app.add_page("Modeling and Evaluation", page_modeling_body)
+app.add_page("House Price Prediction", page_prediction_body)
+app.add_page("Feedback and Reporting", page_reporting_body)
